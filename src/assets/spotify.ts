e98b8170c2fff73d70becffea3497a4060ae0ae5
Isreal6ix <isrealsix@gmail.com>
2021-08-10 21:21:24 +0700

    installed react-router-dom and installed the types for typescript

--- src/assets/spotify.ts
+++ src/assets/spotify.ts
@@ -1,3 +1,5 @@
+import { useLocation } from 'react-router-dom';
+
 // Redirect to Spotify login page
 // Then returns back to our App
 
@@ -16,8 +18,12 @@ const scopes = [
 	'user-modify-playback-state',
 ];
 
-export const getTokenFromUrl = () => {
-	return;
+export const GetTokenFromUrl = () => {
+	const location = useLocation();
+	const queryParams = new URLSearchParams(location.search)
+	const clientId = queryParams.get('client_id')
+	console.log(clientId)
+	return clientId;
 };
 
 export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
